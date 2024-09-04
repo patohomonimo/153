8a293ab56775ad7e54b706b2d447d7eae80162d9
patohomonimo <4553416939@estudante.sed.sc.gov.br>
2024-09-04 18:35:47 -0300

    Add files via upload

--- atividade3.java
+++ atividade3.java
@@ -1,38 +1,42 @@
 import java.util.Scanner;
 
-public class ativi{
-  public static void main(String[] args) {
-    System.out.println("Olá, digite um número inteiro de um a cinco:");
-   
-    Scanner leia = new Scanner(System.in);
-   
-    int numero = leia.nextInt();
+public class atividade3 {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        int dia; 
+        System.out.println("Digite o dia: ");
+        dia = leia.nextInt();
+        switch (dia) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+                
+            case 2:
+                System.out.println("Segunda-feira");
+                break;
 
-    
-    switch (numero){
-      case 1:
-      System.out.print("Número um foi digitado...");
-      break;
+            case 3:
+                System.out.println("Terça-feira");
+                break;
 
-      case 2:
-      System.out.print("Número dois foi digitado...");
-      break;
+            case 4:
+                System.out.println("Quarta-feira");
+                break;
 
-      case 3:
-      System.out.print("Número três foi digitado...");
-      break;
+            case 5:
+                System.out.println("Quinta-feira");
+                break;
 
-      case 4:
-      System.out.print("Número quatro foi digitado...");
-      break;
+            case 6:
+                System.out.println("Sexta-feira");
+                break;
 
-      case 5:
-      System.out.print("Número cinco foi digitado...");
-      break;
+            case 7:
+                System.out.println("Sabado");
+                break;
 
-      default:
-      System.out.print("Número inválido");
-      break;
+            default:
+                System.out.println("numero invalido");
+        }
     }
-  }
 }
